7ec7705d091d40edf244a97627dc9d5c77b3f362
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-10-06 18:47:26 +0700

    add dictionary example

--- contoh-dictionary.py
+++ contoh-dictionary.py
@@ -1,11 +1,20 @@
 print('contoh dictionary ---------------------')
+# {'key1': 'value1', 'key2': 'value2'}
 orang = {
     'nama': 'Dono',
     'alamat': 'Jakarta',
     'hobby': 'Memancing'
 }
-# kurung kurawal: curly brace
+orang['pendidikan'] = 'D3'
 print(orang)
 print(orang['nama'])
 print(orang['alamat'])
 print(orang['hobby'])
+
+print('parse dictionary ---------------------')
+for key in orang:
+    print(key, orang[key])
+
+print('parse dictionary (cara lain) --------')
+for key, value in orang.items():
+    print(key, value)
